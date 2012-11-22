df84b2044310108c975555545ee7bd30a62f5cdb
Brian Quaranto <quaranto@gmail.com>
2012-11-21 20:06:05 -0500

    Tabula rasa for rapid re-prototyping

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcatqd::Application.config.secret_token = '0b6217f7bc36d772bdd150f68a97e2ff74a9ff434fd129022b0309a3b5749fbafbb2f0d72ab3a3d9939a8046d3c47117a26271669e7b6fcaf438f6955eec3868'
