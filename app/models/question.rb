95ccc18b22812ab325ee63a11059f46bcb0f4910
Brian Quaranto <quaranto@gmail.com>
2013-01-31 23:42:32 -0500

    Documentation for question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -13,8 +13,9 @@ class Question < ActiveRecord::Base
   #Question-Option Relationship
   has_many :options, :dependent => :destroy
   
+  #Accept Nested Attributes:
   accepts_nested_attributes_for :options, :answers, :category, :subcategory
   
-  self.per_page = 10
-
+  #Define Question#index pagination rule
+  self.per_page = 6
 end
