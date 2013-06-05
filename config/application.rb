804b2d13bde67b3ebb995eab20eb52a342809f26
Brian Quaranto <quaranto@gmail.com>
2013-06-04 22:48:17 -0400

    fix heroku slug compliation error

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module Mcatqd
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # Fix conflict with Heroku slug compilation
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
